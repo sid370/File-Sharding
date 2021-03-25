3153dd7f17267ee2a45f3ac32827c2a442790014
Sidddhant Tiwary <siddhanttiwary30@gmail.com>
2021-03-25 09:42:12 +0530

    file generation created

--- blockchainLedger.js
+++ blockchainLedger.js
@@ -9,7 +9,7 @@ const morgan = require("morgan")
 const cors = require("cors")
 
 class CryptoBlock{
-    constructor(index,timestamp,owner,data,fileNumber,precedingHash='',nextHash=''){
+    constructor(index,timestamp,owner,data,fileNumber,order,precedingHash='',nextHash=''){
         this.index=index
         this.timestamp=timestamp
         this.owner=owner
@@ -17,6 +17,7 @@ class CryptoBlock{
         this.data=cryptr.encrypt(data)
         this.hash=this.computeHash()
         this.fno=fileNumber
+        this.order=order
     }
     
     computeHash(){
@@ -29,6 +30,7 @@ class Blockchain{
         let data
         (fs.existsSync('blockchain'))? data = fs.readFileSync('blockchain',{encoding: 'utf-8'}):null
         data?  this.blockchain=JSON.parse(data) : this.blockchain = [this.genesisBlock()]
+        fs.writeFileSync('blockchain',JSON.stringify(this.blockchain))
         //this.blockchain = [this.genesisBlock()]
     }
 
@@ -48,7 +50,6 @@ class Blockchain{
         newBlock.precedingHash=this.obtainLatestBlock().hash
         this.obtainLatestBlock().nextHash=newBlock.hash
         this.blockchain.push(newBlock)
-        //fs.unlinkSync('blockchain.txt')
         fs.writeFileSync('blockchain',JSON.stringify(this.blockchain))
     }
 
@@ -56,9 +57,9 @@ class Blockchain{
         return this.obtainLatestBlock().index
     }
 
-    getData(hash){
+    getData(fno){
         for (var i=0;i<this.blockchain.length;i++){
-            if (this.blockchain[i].hash===hash){
+            if (this.blockchain[i].fno===fno){
                 return {
                     "data": cryptr.decrypt(this.blockchain[i].data)
                 }
@@ -70,9 +71,6 @@ class Blockchain{
 
 let fileHandler = new Blockchain()
 
-//fileHandler.addNewBlock(new CryptoBlock(1,new Date(),'Saksham Gupta','Sakshams data'))
-
-//console.log(JSON.stringify(fileHandler,null,4))
 
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -80,7 +78,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/addBlock',async (req,res,next)=>{
-    await fileHandler.addNewBlock(new CryptoBlock(fileHandler.getIndex()+1,new Date(),req.body.owner,req.body.data,req.body.fno))
+    await fileHandler.addNewBlock(new CryptoBlock(fileHandler.getIndex()+1,new Date(),req.body.owner,req.body.data,req.body.fno,req.body.order))
     res.status(200).json({
         message: 'successful'
     })
@@ -92,8 +90,18 @@ app.get('/getChain',(req,res,next)=>{
     })
 })
 
-app.get('/getChain/:hash',(req,res,next)=>{
-    var data = fileHandler.getData(req.params.hash)
+app.post('/clearChain',(req,res,next)=>{
+    fileHandler.blockchain=[fileHandler.genesisBlock()]
+    fs.writeFileSync('blockchain',JSON.stringify(fileHandler.blockchain))
+    res.status(200).json({
+        message: 'Blockchain cleared'
+    })
+})
+
+app.get('/getChain/:fno',(req,res,next)=>{
+    var data = fileHandler.getData(req.params.fno)
+    console
+    console.log(data)
     res.status(200).json({
         blockData: data
     })
@@ -106,6 +114,13 @@ app.get('/checkValid',(req,res,next)=>{
     })
     else res.status(200).json({
         message: 'Chain Compromised'
+    })
+})
+
+app.get('/chainLength',(req,res,next)=>{
+    var length = fileHandler.blockchain.length
+    res.status(200).json({
+        length
     })
 })
 
--- fileServer.js
+++ fileServer.js
@@ -9,6 +9,11 @@ const splitFile = require('split-file')
 const { spawn } = require('child_process')
 global.fetch = require("node-fetch");
 const SHA256 = require("crypto-js/sha256")
+const { response } = require('express')
+const Cryptr = require("cryptr")
+const { pathToFileURL } = require('url')
+const cryptr = new Cryptr("SecretKey10")
+var path = require("path")
 
 app.use(morgan("dev"))
 app.use(cors())
@@ -28,55 +33,63 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage })
 
+function deleteFiles(array) {
+    for (i = 0; i < array.length; i++) {
+        fs.unlinkSync(array[i])
+    }
+}
+
 app.post('/file', upload.single('file'), async (req, res, next) => {
 
     var data = ''
 
     let uniqueName = ''
+
+    nameArray = []
+    nameArray.push('./uploads/' + fileName)
     const cmd = await spawn('split-file', ['-s', './uploads/' + fileName, 3])
     cmd.stdout.on('data', (data) => {
         console.log(`Data: ${data}`)
-        
-        for (var i = 0; i < 3; i++) {
-            var name = './uploads/'+fileName + '.sf-part' + (i + 1)
-            let fname = SHA256(fileName).toString()
-            uniqueName=fname
-            fs.readFile(name, 'utf8',  (err, val) => {
-                // if (err) return res.status(404).json({
-                //     message: 'some error occured'
-                // })
 
-                if (err)    console.warn(err)
+        for (var i = 0; i < 3; i++) {
+
+            var name = './uploads/' + fileName + '.sf-part' + (i + 1)
+            nameArray.push(name)
+
+            let fname = SHA256(name + new Date()).toString()
+            uniqueName += " " + fname
+
+            fs.readFile(name, 'utf8', (err, val) => {
+                if (err) console.warn(err)
                 let bodyData = val
-                //console.log(bodyData)
-                //var myHeaders = new Headers();
-                //myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-                
-                bodyData={
+
+                bodyData = {
                     owner: req.body.owner,
                     data: bodyData,
                     fno: fname
                 }
 
-                console.log('Data: ',bodyData)
+                console.log('Data: ', bodyData)
                 var requestOptions = {
                     method: 'POST',
                     mode: 'cors',
-                    headers:{
+                    headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(bodyData)
                 };
 
                 fetch("http://localhost:3000/addBlock", requestOptions)
-                    .then(response => response.text())
-                    .then(result => console.log(result))
+                    .then(response => response.json())
+                    .then(result => {
+                        console.log(result)
+                    })
                     .catch(error => console.log('error', error));
             })
-
-
-
         }
+
+        deleteFiles(nameArray)
+
     })
 
     cmd.stderr.on('err', (err) => {
@@ -87,10 +100,72 @@ app.post('/file', upload.single('file'), async (req, res, next) => {
         console.log(`child process exited with code ${code}`);
         res.status(200).json({
             message: 'File Uploaded',
-            UUID: uniqueName
+            UUID: uniqueName.split(" ").join("")
         })
     });
 
 })
+
+
+app.get('/genFile/:uid', async (req, res, next) => {
+    resp = []
+    resp[0] = req.params.uid.slice(0, 64)
+    resp[1] = req.params.uid.slice(64, 128)
+    resp[2] = req.params.uid.slice(128, 192)
+    console.log("Resp " + resp)
+    let fileData = ''
+    fetch('http://localhost:3000/getChain/' + resp[0], {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => response.json())
+        .then(response => {
+            fileData = fileData + response.blockData.data
+            fetch('http://localhost:3000/getChain/' + resp[1], {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+                .then(response => response.json())
+                .then(response => {
+                    fileData = fileData + response.blockData.data
+
+                    fetch('http://localhost:3000/getChain/' + resp[2], {
+                        method: 'GET',
+                        mode: 'cors',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                    })
+                        .then(response => response.json())
+                        .then(async response => {
+                            fileData = fileData + response.blockData.data
+
+                            fs.writeFile("./" + req.params.uid + ".txt",fileData,(err)=>{
+                                if (err)
+                                res.status(404).json({
+                                    message: 'Some error occured while creating the file'
+                                })
+
+                                res.download("./" + req.params.uid + ".txt")
+                            })
+
+                            await res.download("./" + req.params.uid + ".txt")
+
+                            // res.sendFile(req.params.uid + ".txt", { root: path.join(__dirname) }, (err) => {
+                            //     (err)? console.warn(err):null
+                            // })
+                        })
+
+                })
+        })
+
+})
+
 
 app.listen(4000)
